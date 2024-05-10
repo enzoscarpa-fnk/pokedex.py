e613fade698c81cf598e130a1a8c1f5697bb094f
fnk <enzo.apple@me.com>
2024-05-10 21:58:50 +0200

    0.1: Database creation

---
+++ test.py
@@ -0,0 +1,21 @@
+
+# AUDIO CRIES FUNCTION ---------------
+
+c.execute('SELECT * FROM Kanto WHERE PK_Index = 025')
+pikachu = c.fetchone()
+print(pikachu)
+
+cry_qry = ('SELECT Pokemon_CryPath FROM Kanto WHERE PK_Index = 025')
+c.execute(cry_qry)
+crypath=c.fetchone()[0]
+print(crypath)
+
+pygame.mixer.init()
+pygame.mixer.music.load(open(crypath))
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    sleep(1)
+print("done")
+
+
+#  ------------------
